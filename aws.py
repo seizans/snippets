579611ec0c9574b79a7cb2ebb5edddeb4fd2b1af
Seizan Shimazaki <seizans@gmail.com>
2016-11-08 17:23:01 +0900

    Imporve

--- aws.py
+++ aws.py
@@ -11,27 +11,32 @@ def get_token():
     return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(n)])
 
 
-def list_email():
+def list_emails():
     with open('foo.md', 'r') as f:
         reader = csv.reader(f)
         return [row[0] for row in reader]
 
 
 def get_user_name(email):
-    email[: email.find('@aktsk.jp')]
+    return email[: email.find('@aktsk.jp')]
+
+
+def register(client, email):
+    user_name = get_user_name(email)
+    client.create_user(UserName=user_name)
+    password = get_token()
+    client.create_login_profile(UserName=user_name, Password=password, PasswordResetRequired=True)
+    client.add_user_to_group(GroupName='aktsk-member', UserName=user_name)
+    with open('out', 'a') as f:
+        f.write('{},{}\n'.format(user_name, password))
+        print('{}'.format(user_name))
 
 
 def main():
     client = boto3.client('iam')
-    emails = list_email()
+    emails = list_emails()
     for email in emails:
-        user_name = get_user_name(email)
-        client.create_user(UserName=user_name)
-        password = get_token()
-        client.create_profile(UserName=user_name, Password=password, PasswordResetRequired=True)
-        client.add_user_to_group(GroupName='change-password', UserName=user_name)
-        with open('out', 'a') as f:
-            f.write('{},{}\n'.format(user_name, password))
+        register(client, email)
 
 
 if __name__ == '__main__':
